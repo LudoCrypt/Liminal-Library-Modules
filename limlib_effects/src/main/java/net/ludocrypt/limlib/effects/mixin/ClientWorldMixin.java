ddf0ac393b8c60bf74d67bb9cb9fd51b5e304493
LudoCrypt <60561627+LudoCrypt@users.noreply.github.com>
2022-08-25 16:51:50 -0500

    Update ClientWorldMixin.java

--- limlib_effects/src/main/java/net/ludocrypt/limlib/effects/mixin/ClientWorldMixin.java
+++ limlib_effects/src/main/java/net/ludocrypt/limlib/effects/mixin/ClientWorldMixin.java
@@ -4,6 +4,7 @@ import java.util.function.Supplier;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
@@ -20,7 +21,7 @@ import net.minecraft.world.dimension.DimensionType;
 @Mixin(ClientWorld.class)
 public class ClientWorldMixin {
 
-	@Inject(method = "Lnet/minecraft/client/world/ClientWorld;<init>(Lnet/minecraft/client/network/ClientPlayNetworkHandler;Lnet/minecraft/client/world/ClientWorld$Properties;Lnet/minecraft/util/registry/RegistryKey;Lnet/minecraft/util/Holder;IILjava/util/function/Supplier;Lnet/minecraft/client/render/WorldRenderer;ZJ)V", at = @At("HEAD"))
+	@Inject(method = "Lnet/minecraft/client/world/ClientWorld;<init>(Lnet/minecraft/client/network/ClientPlayNetworkHandler;Lnet/minecraft/client/world/ClientWorld$Properties;Lnet/minecraft/util/registry/RegistryKey;Lnet/minecraft/util/Holder;IILjava/util/function/Supplier;Lnet/minecraft/client/render/WorldRenderer;ZJ)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/SkyProperties;byDimensionType(Lnet/minecraft/world/dimension/DimensionType;)Lnet/minecraft/client/render/SkyProperties;", shift = Shift.BEFORE))
 	private void limlib$init(ClientPlayNetworkHandler clientPlayNetworkHandler, ClientWorld.Properties properties, RegistryKey<World> registryKey, Holder<DimensionType> holder, int i, int j, Supplier<Profiler> supplier, WorldRenderer worldRenderer, boolean bl, long l, CallbackInfo ci) {
 		SkyEffects.tempHolder = holder;
 	}

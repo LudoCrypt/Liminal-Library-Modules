47fc6f531de071a1ec83dd1049af1930b7401f49
LudoCrypt <60561627+LudoCrypt@users.noreply.github.com>
2022-08-24 21:29:06 -0500

    Update BuiltinRegistriesMixin.java

--- limlib_registry/src/main/java/net/ludocrypt/limlib/registry/mixin/BuiltinRegistriesMixin.java
+++ limlib_registry/src/main/java/net/ludocrypt/limlib/registry/mixin/BuiltinRegistriesMixin.java
@@ -3,6 +3,7 @@ package net.ludocrypt.limlib.registry.mixin;
 import org.quiltmc.loader.api.QuiltLoader;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
@@ -12,7 +13,7 @@ import net.minecraft.util.registry.BuiltinRegistries;
 @Mixin(BuiltinRegistries.class)
 public class BuiltinRegistriesMixin {
 
-	@Inject(method = "<clinit>", at = @At("HEAD"))
+	@Inject(method = "<clinit>", at = @At(value = "INVOKE", target = "Lcom/google/common/collect/Maps;newLinkedHashMap()Ljava/util/LinkedHashMap;", shift = Shift.AFTER))
 	private static void limlib$clinit(CallbackInfo ci) {
 		QuiltLoader.getEntrypoints(PreRegistration.ENTRYPOINT_KEY, PreRegistration.class).forEach(PreRegistration::register);
 	}
